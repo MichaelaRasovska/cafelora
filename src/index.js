7f1c9d80f940d395ae1e2859cd1dad8326576989
Michaela Rasovska <michaela.rasovska@gmail.com>
2020-11-01 12:06:09 +0100

    Objednávání a rušení kafe

--- src/index.js
+++ src/index.js
@@ -16,3 +16,19 @@ for (let i = 0; i < navLinks.length; i++) {
     navElm.classList.add('nav-closed');
   });
 }
+
+const orderBtn = document.querySelectorAll('.order-btn');
+const drinkCup = document.querySelectorAll('.drink__cup');
+
+for (let i = 0; i < orderBtn.length; i++) {
+  orderBtn[i].addEventListener('click', () => {
+    for (let j = 0; j < drinkCup.length; j++) {
+      drinkCup[j].classList.toggle('drink__cup--selected');
+      if (drinkCup[j].classList.contains('drink__cup--selected')) {
+        orderBtn[i].textContent = 'Zrušit';
+      } else {
+        orderBtn[i].textContent = 'Objednat';
+      }
+    }
+  });
+}
