d0d37d870571158dd54618184ed3c86e36046f9d
Michaela Rasovska <michaela.rasovska@gmail.com>
2020-11-01 11:29:44 +0100

    Schovávání položek v navigaci po kliknutí

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import './index.html';
 import './style.css';
 
 console.log('funguju!');
+
+const navBtn = document.querySelector('#nav-btn');
+navBtn.addEventListener('click', () => {
+  const navElm = document.querySelector('#nav-items');
+  navElm.classList.toggle('nav-closed');
+});
+
+const navLinks = document.querySelectorAll('.nav-links');
+for (let i = 0; i < navLinks.length; i++) {
+  navLinks[i].addEventListener('click', () => {
+    const navElm = document.querySelector('#nav-items');
+    navElm.classList.add('nav-closed');
+  });
+}
